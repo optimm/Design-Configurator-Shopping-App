03af766371fc481e390c5a543daf346a1a80aaeb
optimm <ayushsaxena823@gmail.com>
2022-06-26 10:24:42 +0530

    summary fix

--- src/components/body/summary/Summary.jsx
+++ src/components/body/summary/Summary.jsx
@@ -4,28 +4,28 @@ import { useNavigate } from 'react-router-dom';
 import { clearUser } from '../../../features/user/userSlice';
 import "./summary.css"
 function Summary() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    useEffect(() => {
-        let confirmed = sessionStorage.getItem('confirmed');
-        confirmed = Boolean(confirmed);
-        if (confirmed === true) { }
-        else { navigate("/"); }
-    }, [])
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     let confirmed = sessionStorage.getItem('confirmed');
+    //     confirmed = Boolean(confirmed);
+    //     if (confirmed === true) { }
+    //     else { navigate("/"); }
+    // }, [])
 
-    function handleClean() {
-        sessionStorage.clear();
-        sessionStorage.setItem('isauth', false);
-        sessionStorage.setItem('isempty', true);
-        dispatch(clearUser());
-        navigate("/");
-    }
+    // function handleClean() {
+    //     sessionStorage.clear();
+    //     sessionStorage.setItem('isauth', false);
+    //     sessionStorage.setItem('isempty', true);
+    //     dispatch(clearUser());
+    //     navigate("/");
+    // }
 
 
     return (
         <div className='summary'>
-            <h1>Thank You For Your Order</h1>
-            <button onClick={handleClean}>Exit</button>
+            <h1>Thank you for your order, we'll get back to you soon</h1>
+            <button className="exit-button">Exit</button>
         </div>
     )
 }
